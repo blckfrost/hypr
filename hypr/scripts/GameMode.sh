5f1d0c7c187061c896247d6450ca1c59172858ea
blckfrost <beataanima88@gmail.com>
2025-05-12 23:58:05 +0000

    update hypr config

--- hypr/scripts/GameMode.sh
+++ hypr/scripts/GameMode.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 notif="$HOME/.config/swaync/icons/gamemode.png"
-SCRIPTSDIR="$HOME/.config/hypr/scripts"
 
 
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
@@ -14,16 +13,7 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
-    # swww kill 
-    swww img "$HOME/.config/swww/.current_wallpaper"
     notify-send -e -u low -i "$notif" "gamemode enabled. All animations off"
-    exit
-else
-    swww-daemon --format xrgb
-    swww img "$HOME/.config/swww/.current_wallpaper"
-	sleep 0.1
-    wal -i "$HOME/.config/swww/.current_wallpaper"
-    notify-send -e -u normal -i "$notif" "gamemode disabled. All animations normal"
     exit
 fi
 hyprctl reload
--- hypr/scripts/matugen-apply.sh
+++ hypr/scripts/matugen-apply.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-pywal-discord
-walogram -s > /dev/null
+#pywal-discord
+#walogram -s > /dev/null
 pywalfox update
 spicetify apply -q -n
+
+# set gtk theme
+gsettings set org.gnome.desktop.interface gtk-theme ""
+gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
+
